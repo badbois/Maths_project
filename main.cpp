4cfccb1b745c919f03798208fe2fa40913614426
Yaelle <yaellesaraleguitran@gmail.com>
2022-03-20 12:08:59 +0100

    [main.cpp] random object that changes on click
    Just to get used to p6 and to see what it could look like, but we won't keep the code because we will use classes etc...

--- main.cpp
+++ main.cpp
@@ -1,24 +1,93 @@
 #include <iostream>
 #include <p6/p6.h>
 
+#include <cmath>
+#include <vector>
+
+float randomUniform() { return (float)rand() / RAND_MAX; }
+
+int probaUniform(std::vector<float> probas, std::vector<int> valeurs) {
+  float rand = randomUniform();
+  int i = 0;
+  float proba = probas[0];
+  while (rand > proba && i < probas.size() - 1) {
+    proba += probas[i + 1];
+    i++;
+  }
+  return valeurs[i];
+}
+
+int sensRotation() {
+  std::vector<float> probas;
+  std::vector<int> rotationSens;
+  probas.push_back(0.3);
+  probas.push_back(0.3);
+  probas.push_back(0.4);
+  rotationSens.push_back(-1);
+  rotationSens.push_back(1);
+  rotationSens.push_back(0);
+  return probaUniform(probas, rotationSens);
+}
+
+int formeProbability() {
+  std::vector<float> probas;
+  std::vector<int> forme;
+  probas.push_back(0.3);
+  probas.push_back(0.3);
+  probas.push_back(0.4);
+  forme.push_back(0);
+  forme.push_back(1);
+  forme.push_back(2);
+  return probaUniform(probas, forme);
+}
+
+void drawobject(auto &ctx, float i, float j, p6::Angle rotation, int forme,
+                int rotationSens) {
+  switch (forme) {
+  case 0:
+    ctx.square(p6::Center{i, j}, p6::Radius{0.05f},
+               p6::Rotation{rotationSens * rotation});
+    break;
+  case 1:
+    ctx.circle(p6::Center{i, j}, p6::Radius{0.05f});
+    break;
+  }
+}
+
+void reinitialize(std::vector<int> &formes, std::vector<int> &sens) {
+  for (int i = 0; i < formes.size(); i++) {
+    formes[i] = formeProbability();
+    sens[i] = sensRotation();
+  }
+}
+
 int main() {
   auto ctx = p6::Context{{720, 720, "My p6 project"}};
   float pas = 1 / 4.0;
   p6::Angle rotation = 0.011_turn;
   float time = 0;
+  std::vector<int> formes(8 * 8);
+  std::vector<int> sens(8 * 8);
+  reinitialize(formes, sens);
   ctx.use_stroke = false;
   ctx.update = [&]() {
     ctx.background({0.2f, 0.1f, 0.3f});
+    int k = 0;
     for (float i = -1 + pas; i < 1; i += pas) {
       for (float j = -1 + pas; j < 1; j += pas) {
-        ctx.square(p6::Center{i, j}, p6::Radius{0.05f}, p6::Rotation{rotation});
+        drawobject(ctx, i, j, rotation, formes[k], sens[k]);
+        k++;
+        // ctx.square(p6::Center{i, j}, p6::Radius{0.05f},
+        // p6::Rotation{rotation});
       }
     };
     time = ctx.time();
     rotation = time * 0.1_turn;
     ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.03f});
 
-    ctx.mouse_pressed = [](p6::MouseButton) { std::cout << "Hello World\n"; };
+    ctx.mouse_pressed = [&formes, &sens](p6::MouseButton) {
+      reinitialize(formes, sens);
+    };
   };
   ctx.start();
 }
