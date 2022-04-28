41b4e974f2328d17b50bb6c6afe720247dbe1a55
Roberty Jordan <jordan.roberty@outlook.fr>
2022-04-28 11:36:03 +0200

    [SCORE] Add display_score()

--- src/score.cpp
+++ src/score.cpp
@@ -1,5 +1,5 @@
 #include "score.hpp"
-#include <sstream>
+
 
 void get_scoreboard(std::vector<int>& scoreboard) {
     std::ifstream file;
@@ -16,6 +16,31 @@ void get_scoreboard(std::vector<int>& scoreboard) {
     }
 } 
 
+void display_scoreboard(p6::Context &ctx){
+    std::vector<int> scoreboard;
+    get_scoreboard(scoreboard);
+    ctx.background({0.f, 0.f, 0.f});
+    ctx.fill = {1., 1., 1., 1};
+    std::vector<std::u16string> scores;
+    for(int i = 0; i < scoreboard.size(); i++) {
+        std::string score_string = std::to_string(scoreboard[i]);
+    std::u16string score_u16string = std::u16string(score_string.begin(),
+                                                    score_string.end());
+        scores.push_back(score_u16string);
+    }
+    ctx.text_size = 0.04f;
+    ctx.text(u"Scoreboard", p6::Center{0.f, 0.4f});
+    ctx.text(u"1: "+ scores[0], p6::Center{0.f, 0.2f});
+    ctx.text(u"2: "+ scores[1], p6::Center{0.f, 0.f});
+    ctx.text(u"3: "+ scores[2], p6::Center{0.f, -0.2f});
+
+    ctx.text_size = 0.02f;
+    ctx.text(u"retour:M", p6::Center{0.8f, 0.9f});
+
+
+
+    
+}
 
 void add_new_score(const int score){
     std::vector<int> scoreboard;
--- src/score.hpp
+++ src/score.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <p6/p6.h>
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+
 
 void get_scoreboard(std::vector<int>& scoreboard);
+void display_scoreboard(p6::Context &ctx);
 void add_new_score(const int scores);
