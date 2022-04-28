c06b9eeacc43ebff3776eefc3062921e29a2a04f
Roberty Jordan <jordan.roberty@outlook.fr>
2022-04-28 11:37:05 +0200

    [MAIN/MENU] Add display complete menu with effective scoreboard

--- src/main.cpp
+++ src/main.cpp
@@ -8,45 +8,57 @@
 #include "game.hpp"
 #include "score.hpp"
 
-void show_menu(p6::Context &ctx, bool &is_playing, float &game_start_time) {
+void show_menu(p6::Context &ctx, bool &is_playing, bool &is_scoreboard,  float &game_start_time) {
   ctx.text_size= 0.05f;
-  ctx.text(u"Prout le pet", p6::Center{0.f, 0.f});
+  ctx.text(u"New game:P", p6::Center{0.f, 0.6f});
+  ctx.text(u"Scoreboard:S", p6::Center{0.f, 0.2f});
+  ctx.text(u"Rules:R", p6::Center{0.f, -0.2f});
+  ctx.text(u"Quit:Q", p6::Center{0.f, -0.6f});
+
   ctx.key_pressed = [&](p6::Key key) {
     std::cout << key.logical << std::endl;
     if (key.logical == "q") {
       ctx.stop();
     } else if (key.logical == "p") {
-      is_playing = true;
-      game_start_time = ctx.time();
+      if(!is_playing && !is_scoreboard){
+        is_playing = true;
+        game_start_time = ctx.time();
+      }
     }else if (key.logical == "s") {
-      //affiche le score dans la console, pour le moment
-      std::vector<int> scoreboard;
-      get_scoreboard(scoreboard);
-      for(int i = 0; i < scoreboard.size(); i++) {
-        std::cout << scoreboard[i] << std::endl;
+      if(!is_playing){
+        is_scoreboard = true;
+      }
+    }
+    if(is_scoreboard){
+      if(key.logical == "m"){
+        is_scoreboard = false;
       }
     }
   };
-}
+ };
+
 
 int main() {
   try {
-    auto ctx = p6::Context{{720, 720, "My p6 project"}};
+    auto ctx = p6::Context{{720, 720, "Unique project"}};
     ctx.time_perceived_as_realtime();
     float game_start_time = 0;
 
     ctx.use_stroke = false;
     bool is_playing = false;
+    bool is_scoreboard = false;
     set_game_grid();
 
     // Main loop
     ctx.update = [&]() {
-      ctx.background({0.2f, 0.1f, 0.3f});
-      ctx.fill = {1., 1., 1., 0.5};
-      if (is_playing) {
+      ctx.background({0.f, 0.f, 0.f});
+      ctx.fill = {1., 1., 1., 1};
+      if (is_playing && !is_scoreboard) {
         play_game(ctx, is_playing, game_start_time);
-      } else {
-        show_menu(ctx, is_playing, game_start_time);
+      } else if(!is_playing && !is_scoreboard) {
+        show_menu(ctx, is_playing, is_scoreboard , game_start_time);
+      } else if(is_scoreboard) {
+        display_scoreboard(ctx);
       }
     };
     ctx.start();
