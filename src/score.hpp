62816943fb90425497b7afeb62a611dcd1172c3f
Roberty Jordan <jordan.roberty@outlook.fr>
2022-05-11 16:20:01 +0200

    [SCORE] Return vector in get_scoreboard

--- src/score.hpp
+++ src/score.hpp
@@ -2,11 +2,6 @@
 
 #include <p6/p6.h>
 #include <vector>
-#include <iostream>
-#include <fstream>
-#include <sstream>
 
-
-void get_scoreboard(std::vector<int>& scoreboard);
 void display_scoreboard(p6::Context &ctx);
 void add_new_score(const int scores);
